b2dcd3036520c325e8e29c5d256eb3e21a5aa0e4
Sibozhu <siboz@bu.edu>
2017-09-25 15:29:00 -0400

    wew

--- 9.22 lecture.R
+++ 9.22 lecture.R
@@ -5,3 +5,12 @@ library(nycflights13)
 ?nycflights13
 
 flights
+
+starwars
+
+filter(starwars,species=="Human")
+filter(starwars,mass > 1000)
+
+arrange(mtcars,cyl,disp)
+arrange(mtcars,desc(disp))
+
