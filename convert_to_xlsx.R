21ca1e7edac406df361281f8532035bd15d19b3d
Sibozhu <siboz@bu.edu>
2017-11-06 17:08:50 -0500

    update

--- convert_to_xlsx.R
+++
@@ -1,9 +0,0 @@
-library(openxlsx)
-
-
-#writing data into xlsx file
-for (i in 2001:2016) {
-  YEAR = i
-  load(paste0("./cleaned_data/ave_temp_",YEAR,".Rdata"))
-  write.xlsx(data.output,file = "Sub_continent_West_data_"+YEAR,sheetName="Sheet1",colnames=TRUE,row.names=FALSE,append=FALSE,showNA=TRUE)
-}
