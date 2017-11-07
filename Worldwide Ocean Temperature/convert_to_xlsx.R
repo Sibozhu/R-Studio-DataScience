ff94aa23ad947844e997790894a2bffc11501c78
Sibozhu <siboz@bu.edu>
2017-11-06 19:13:29 -0500

    create xlsx file

--- Worldwide Ocean Temperature/convert_to_xlsx.R
+++ Worldwide Ocean Temperature/convert_to_xlsx.R
@@ -10,7 +10,7 @@ library(openxlsx)
   #$write.xlsx(data.output,file = "Sub_continent_West_data_"+YEAR,sheetName="Sheet1",colnames=TRUE,row.names=FALSE,append=FALSE,showNA=TRUE)
 #}
 
-a <- load("./Cleaned_Data/ave_temp_2001.Rdata")
+a <- load("./Worldwide Ocean Temperature/Cleaned_Data/cleaned_2002.Rdata")
 
-write.xlsx(a,file = "testing.xlsx",sheetName="Sheet1",colnames=TRUE,row.names=FALSE,append=FALSE,showNA=TRUE)
+write.xlsx(a,file = "testing.xlsx",sheetName="cleaned",colnames=TRUE,row.names=FALSE,append=FALSE,showNA=TRUE)
 
